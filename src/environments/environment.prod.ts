59b28a188cd02cc1f7f5500681e714904cf8e75d
Andreas Gassmann <andreas@andreasgassmann.ch>
2021-02-02 00:28:47 +0000

    fix(env): use dev values for prod deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
   production: true,
+  tzColorsContract: 'KT1NiEWQgGmkyXbXFjViXbLghia6neMMyia3',
+  tzColorsAuctionContract: 'KT1NqoJ7FubjmmBu75wHvTVBZ2WDeJdXQfRP',
+  rpcUrl: 'https://testnet-tezos.giganode.io',
+  appName: 'TzColors',
+  proxyUrl: 'https://cors-proxy.airgap.prod.gke.papers.tech/proxy?url=',
+  colorsBigmapUrl:
+    'https://better-call.dev/v1/bigmap/delphinet/53849/keys?q=&offset=0',
+  auctionBigmapUrl:
+    'https://better-call.dev/v1/bigmap/delphinet/53847/keys?q=&offset=0',
 }
