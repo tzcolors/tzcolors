532999efb733ed7eab333edceaed39c0401ef037
Pascal Brun <brunpasc@gmail.com>
2021-02-02 16:55:25 +0100

    feat(app): consistent naming

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   tzColorsContract: 'KT18tARKsAFDKDzGK1Rn8b1XMRQ13qM1X8Ra',
   tzColorsAuctionContract: 'KT1FM43QFTg8YGXBo3Z87VoFVJiwDSeoQxmg',
   rpcUrl: 'https://testnet-tezos.giganode.io',
-  appName: 'TzColors',
+  appName: 'tzcolors',
   proxyUrl: 'https://cors-proxy.airgap.prod.gke.papers.tech/proxy?url=',
   colorsBigmapUrl:
     'https://better-call.dev/v1/bigmap/delphinet/54998/keys?q=&offset=0',
