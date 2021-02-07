40d48219cf1d6cffc96db054b6ef1dc4a0b0f467
Andreas Gassmann <andreas@andreasgassmann.ch>
2021-02-07 13:19:43 +0000

    feat(contract): update to latest contract version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
-  tzColorsContract: 'KT18tARKsAFDKDzGK1Rn8b1XMRQ13qM1X8Ra',
-  tzColorsAuctionContract: 'KT1FM43QFTg8YGXBo3Z87VoFVJiwDSeoQxmg',
+  tzColorsContract: 'KT1HUpMGp2yH81smMhV54qDg9Kzhhs5C7q3T',
+  tzColorsAuctionContract: 'KT1QRs3Q2vTDu6JJX1XeDJPCbkX8BHRuGs1E',
   rpcUrl: 'https://testnet-tezos.giganode.io',
   appName: 'tzcolors',
   proxyUrl: 'https://cors-proxy.airgap.prod.gke.papers.tech/proxy?url=',
   colorsBigmapUrl:
-    'https://better-call.dev/v1/bigmap/delphinet/54998/keys?q=&offset=0&size=100',
+    'https://better-call.dev/v1/bigmap/delphinet/60541/keys?q=&offset=0&size=100',
   auctionBigmapUrl:
-    'https://better-call.dev/v1/bigmap/delphinet/54996/keys?q=&offset=0&size=100',
+    'https://better-call.dev/v1/bigmap/delphinet/60539/keys?q=&offset=0&size=100',
 }
