1f50fe9fafe408cbbca7a9cba796c1319206aad2
Andreas Gassmann <andreas@andreasgassmann.ch>
2021-02-08 21:47:31 +0000

    feat(landing): replace placeholder auctions

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core'
-import { Router } from '@angular/router'
+import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
+import { Color, StoreService } from 'src/app/services/store/store.service'
 
 @Component({
   selector: 'app-landing',
@@ -7,7 +9,17 @@ import { Router } from '@angular/router'
   styleUrls: ['./landing.component.scss'],
 })
 export class LandingComponent implements OnInit {
-  constructor(private readonly router: Router) {}
+  public colors$: Observable<Color[]> = new Observable()
+  public colorsCount$: Observable<number> = new Observable()
+
+  constructor(private readonly storeService: StoreService) {
+    this.storeService.resetFilters()
+    this.storeService.setView('auctions')
+    this.storeService.setSortType('time')
+    this.storeService.setSortDirection('asc')
+    this.colors$ = this.storeService.colors$.pipe(map((m) => m.slice(0, 7)))
+    this.colorsCount$ = this.storeService.colorsCount$
+  }
 
   ngOnInit(): void {}
 }
