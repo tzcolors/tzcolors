460ec7334c4279b28051e58981519615970f6784
Andreas Gassmann <andreas@andreasgassmann.ch>
2021-02-11 15:05:02 +0000

    feat(contract): new contract address

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
-  tzColorsContract: 'KT1Ho16cRws1yJh7Fa2vVqempfdndh35iEvD',
-  tzColorsAuctionContract: 'KT1T5k439iHSoJKPNYKrWuvHV6W5aXW6EhTC',
+  tzColorsContract: 'KT1AD1dBFQbzFAXQpFDVoBCwJ3Rmsi4MzXqD',
+  tzColorsAuctionContract: 'KT1TPNBKW9xbBjciUPzWCe8haSoYA5zLgJCB',
   rpcUrl: 'https://testnet-tezos.giganode.io',
   appName: 'tzcolors',
   proxyUrl: 'https://cors-proxy.airgap.prod.gke.papers.tech/proxy?url=',
   colorsBigmapUrl:
-    'https://better-call.dev/v1/bigmap/delphinet/64916/keys?q=&offset=0&size=100',
+    'https://better-call.dev/v1/bigmap/delphinet/64934/keys?q=&offset=0&size=100',
   auctionBigmapUrl:
-    'https://better-call.dev/v1/bigmap/delphinet/64914/keys?q=&offset=0&size=100',
+    'https://better-call.dev/v1/bigmap/delphinet/64932/keys?q=&offset=0&size=100',
 }
