9f0f9763e7426f4e54cf904fc9a848511ae3fa30
Andreas Gassmann <andreas@andreasgassmann.ch>
2021-02-11 14:33:31 +0000

    feat(contract): new testnet contracts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,15 @@
 
 export const environment = {
   production: false,
-  tzColorsContract: 'KT1HUpMGp2yH81smMhV54qDg9Kzhhs5C7q3T',
-  tzColorsAuctionContract: 'KT1QRs3Q2vTDu6JJX1XeDJPCbkX8BHRuGs1E',
+  tzColorsContract: 'KT1Ho16cRws1yJh7Fa2vVqempfdndh35iEvD',
+  tzColorsAuctionContract: 'KT1T5k439iHSoJKPNYKrWuvHV6W5aXW6EhTC',
   rpcUrl: 'https://testnet-tezos.giganode.io',
   appName: 'tzcolors',
   proxyUrl: 'https://cors-proxy.airgap.prod.gke.papers.tech/proxy?url=',
   colorsBigmapUrl:
-    'https://better-call.dev/v1/bigmap/delphinet/60541/keys?q=&offset=0&size=100',
+    'https://better-call.dev/v1/bigmap/delphinet/64916/keys?q=&offset=0&size=100',
   auctionBigmapUrl:
-    'https://better-call.dev/v1/bigmap/delphinet/60539/keys?q=&offset=0&size=100',
+    'https://better-call.dev/v1/bigmap/delphinet/64914/keys?q=&offset=0&size=100',
 }
 
 /*
