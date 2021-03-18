2be6cf1b91485c3c05f101b29d0fe9cb908e8136
Pascal Brun <brunpasc@gmail.com>
2021-03-18 15:45:21 +0100

    feat(app-routing): always scroll to top on link click

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,7 +23,7 @@ const routes: Routes = [
 ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top' })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
